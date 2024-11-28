34626f6fd42d04ccc445f8599456942dc1fb3b14
Frank <frank2411@gmail.com>
2024-11-28 13:23:13 +0800

    UPDATE 1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -29,11 +29,35 @@ VALUES
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
+UPDATE "USER"
+SET
+    role = 'COACH'
+WHERE email IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
+AND role = 'USER'
+;
+
+
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
+
+DELETE
+FROM "USER"
+WHERE email = 'opacity0@hexschooltest.io'
+;
+
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
+SELECT COUNT(*) AS "目前所有用戶數量"
+FROM "USER"
+;
+
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+
+SELECT *
+FROM "USER"
+LIMIT 3
+;
 
 
 --  ████████  █████   █    ████
